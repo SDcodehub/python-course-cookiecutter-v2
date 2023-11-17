01a5a2a87e0e611184a712c324476c60a088b2e0
Sagar <sagardesaee@gmail.com>
2023-11-17 20:46:51 +0530

    project creation in test and assert on exists added, fixed linting extension by introducing pyproject.toml file

---
+++ tests/test__generate_project.py
@@ -0,0 +1,35 @@
+import json
+import subprocess
+from contextlib import AbstractAsyncContextManager
+from decimal import DefaultContext
+from pathlib import Path
+
+THIS_DIR = Path(__file__).parent
+PROJECT_DIR = (THIS_DIR / "../").resolve()
+
+def test__can_generate_project():
+    """
+    execute: 'cookiecutter <template directory>  ...
+    """
+
+    cookiecutter_config = {
+        "default_context": {"repo_name": "test-repo"}
+    }
+    cookiecutter_config_fpath = PROJECT_DIR / "tests/cookiecutter-test-config.json"
+
+    cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
+
+    subprocess.run([
+        "cookiecutter",
+        str(PROJECT_DIR),
+        "--output-dir",
+        str(PROJECT_DIR / "sample"),
+        "--no-input",
+        "--config-file",
+        str(cookiecutter_config_fpath),
+
+    ], check=True)
+
+    generate_project_dir = PROJECT_DIR/"sample"/cookiecutter_config["default_context"]["repo_name"] 
+    print(f"{generate_project_dir=}")
+    assert generate_project_dir.exists()
